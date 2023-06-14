6b58d40c1bfdc5b888d0870dd7ece2b6c51faca7
vinni <vinni0159@conestogac.on.ca>
2023-06-14 16:32:50 +0530

    added github link and comments added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Portfolio", navigation: "" });
+  // projects data object
+  res.render("index", { title: "Portfolio", projects: [
+    {
+      projectName: "Weather Application",
+        projectDescription:
+          "A web-app based on Express and React which have beautiful graphis to show weather.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+    },
+    {
+      projectName: "ToDo Web app",
+      projectDescription:
+        "A web-app based on Express and React which stores the tasks you have todo.",
+      projectImage: "/images/laptop-g07bca914f_1280.png",
+    },
+    {
+      projectName: "Shopping Website",
+      projectDescription:
+        "Build with node and react with a fully scalable architecture and customizable dashboard.",
+      projectImage: "/images/laptop-g07bca914f_1280.png",
+    },
+  ] });
 });
 
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET projects listing. */
 router.get("/", function (req, res, next) {
-  res.render("projects");
+  // projects data object
+  res.render("projects", {
+    projects: [
+      {
+        projectName: "Weather Application",
+        projectDescription:
+          "A web-app based on Express and React which have beautiful graphis to show weather.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+      },
+      {
+        projectName: "ToDo Web app",
+        projectDescription:
+          "A web-app based on Express and React which stores the tasks you have todo.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+      },
+      {
+        projectName: "Shopping Website",
+        projectDescription:
+          "Build with node and react with a fully scalable architecture and customizable dashboard.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+      },
+    ],
+  });
 });
 
 module.exports = router;
